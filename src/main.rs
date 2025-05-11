3c6a5d197b9541fff87a8a1ac7d200455314ebf6
Culver Stone <culver.stone57@gmail.com>
2025-05-10 20:41:46 -0700

    Finished Looping

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,97 @@
 //use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    let number = 7;
 
-    another_function(5);
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 
-    print_labeled_measurement(5, 'h');
+    if number != 0 {
+        println!("number was three");
+    }
 
-    print_statement_v_expression();
+    let number = 6;
 
-    let mut x = five();
-    println!("The value of x is: {x}");
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 
-    x = plus_one(x);
-    println!("The value of x is: {x}");
-}
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
-}
+    println!("The value of number is: {number}");
 
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
-}
+    let mut counter = 0;
 
-fn print_statement_v_expression() {
-    let y = {
-        let x = 3;
-        x + 1
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
     };
 
-    println!("The value of y is: {y}");
-}
+    println!("The result is {result}");
 
-fn five() -> i32 {
-    5
-}
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
 
-fn plus_one(x: i32) -> i32 {
-    x + 1
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }

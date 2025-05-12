5eacfe379390db06a385c1ac5e7cdb31811751f5
Culver Stone <culver.stone57@gmail.com>
2025-05-11 20:30:56 -0700

    Finished references

--- src/main.rs
+++ src/main.rs
@@ -3,90 +3,72 @@
 //use rand::Rng;
 
 fn main() {
-                        // s is not valid here, it’s not yet declared
-    let s = "hello";   // s is valid from this point forward
-    // do stuff with s
-
-    let mut s = String::from("hello");
-
-    s.push_str(", world!"); // push_str() appends a literal to a String
-
-    println!("{s}"); // This will print `hello, world!`
-
-
     let s1 = String::from("hello");
-    //let s2 = s1;  // throws an error
-    println!("{s1}, world!");
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+
+    //let s = String::from("hello");
+    //change(&s);
+    let mut s = String::from("hello");
+    change2(&mut s);
 
 
     let mut s = String::from("hello");
-    s = String::from("ahoy");
-    println!("{s}, world!");
+    let r1 = &mut s;
+    //let r2 = &mut s;
+    //println!("{}, {}", r1, r2);
 
 
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-
-    println!("s1 = {s1}, s2 = {s2}");
-
-
-    let s = String::from("hello");  // s comes into scope
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
-    let x = 5;                      // x comes into scope
-    makes_copy(x);                  // because i32 implements the Copy trait,
-                                    // x does NOT move into the function,
-    println!("{}", x);              // so it's okay to use x afterward
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r2 = &mut s;
 
 
-    let s1 = gives_ownership();         // gives_ownership moves its return
-        // value into s1
-    let s2 = String::from("hello");     // s2 comes into scope
-    let s3 = takes_and_gives_back(s2);  // s2 is moved into
-        // takes_and_gives_back, which also
-        // moves its return value into s3
-
-        
-    let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{s2}' is {len}.");
-}                      // this scope is now over, and s is no longer valid
-  // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
-  // happens. s1 goes out of scope and is dropped.
+    let mut s = String::from("hello");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    //let r3 = &mut s; // BIG PROBLEM
+    //println!("{}, {}, and {}", r1, r2, r3);
 
 
-
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{some_string}");
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
-  
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{some_integer}");
-} // Here, some_integer goes out of scope. Nothing special happens.
+    let mut s = String::from("hello");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // variables r1 and r2 will not be used after this point
+    let r3 = &mut s; // no problem
+    println!("{r3}");
 
 
+    let reference_to_nothing = dangle();
+}
 
-fn gives_ownership() -> String {             // gives_ownership will move its
-    // return value into the function
-    // that calls it
+fn calculate_length(s: &String) -> usize {
+    s.len()
+} // Here, s goes out of scope. But because s does not have ownership of what
+  // it refers to, the value is not dropped.
 
-    let some_string = String::from("yours"); // some_string comes into scope
+//fn change(some_string: &String) {
+//    some_string.push_str(", world");
+//} // Illegal since you can't change a variable that's outside of your scope
 
-    some_string                              // some_string is returned and
-    // moves out to the calling
-    // function
+fn change2(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 
-// This function takes a String and returns one
-fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
-             // scope
-    a_string  // a_string is returned and moves out to the calling function
-}
+fn dangle() -> &String { // dangle returns a reference to a String
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
+    let s = String::from("hello"); // s is a new String
 
-    (s, length)
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
+
+  fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
 }
